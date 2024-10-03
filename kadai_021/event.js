0ac353ac4a01e08cb173629dbbe950608ebf7f9a
Ryota37127 <dream_hbb_champ@yahoo.co.jp>
2024-10-03 23:37:49 +0900

    kadai_021を追加

---
+++ kadai_021/event.js
@@ -0,0 +1,10 @@
+const text = document.getElementById("text");
+const btn = document.getElementById("btn");
+
+btn.addEventListener('click',()=>{
+
+setTimeout(()=>{
+    console.log("ボタンをクリックしました");
+},2000);
+
+});
